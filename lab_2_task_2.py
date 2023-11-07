2c70869f0813042bc1ee90de6c4125c6b9ec452d
JackEDay <111586133+JackEDay@users.noreply.github.com>
2023-11-07 13:01:05 +0200

    gnn

--- lab_2_task_2.py
+++ lab_2_task_2.py
@@ -2,5 +2,5 @@ b = int(input('Введите первый член:  '))
 q = int(input('Введите знаменатель:  '))
 n = int(input('Введите кол-во членов:   '))
 for i in range (1, n) :
-    s = (b * (1 - q**i))/(1-q)
+    s = b*(q**(i-1))
     print(s)
--- lab_2_task_6.py
+++ lab_2_task_6.py
@@ -1,4 +1,4 @@
 for i in range (1, 10):
-    print('')
+    print()
     for a in range (1, 10):
         print(a * i, end=' ')
