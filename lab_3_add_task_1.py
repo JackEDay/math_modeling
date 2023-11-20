93cdeee1fc2b3bfce0c107b09f65878ab3a73d75
JackEDay <111586133+JackEDay@users.noreply.github.com>
2023-11-20 22:18:32 +0000

    gemgklerlmelk

--- lab_3_add_task_1.py
+++ lab_3_add_task_1.py
@@ -1,12 +1,40 @@
 import numpy as np
 
-frst_ar_h = input("Размер первого:      ")
-frst_ar_x = input()
+ar_y = int(input("Размер массива:      "))
+ar_x = int(input())
 
-sec_ar_h = input("Размер второго:       ")
-sec_ar_x = input()
+frst_ar = np.zeros((ar_y, ar_x))
+sec_ar = np.zeros((ar_y, ar_x))
+thrd_ar = np.zeros((ar_y, ar_x))
 
-frst_ar = np.zeros((frst_ar_h, frst_ar_x))
-sec_ar = np.zeros((sec_ar_h, sec_ar_x))
+for i in range (0, ar_y):
+    for g in range (0, ar_x):
+        a = int(input())
+        frst_ar[i, g] = a
+        g += 1
+    g = 0
+    i += 1
 
-for i in range 
+i = 0
+g = 0
+
+for i in range (0, ar_y):
+    for g in range (0, ar_x):
+        a = int(input())
+        sec_ar[i, g] = a
+        g += 1
+    g = 0
+    i += 1
+    
+for k in range (0, ar_y):
+    for t in range (0, ar_x):
+        if frst_ar[k, t] >= sec_ar[k, t]:
+            thrd_ar[k, t] = frst_ar[k, t]
+        else:
+            thrd_ar[k, t] = sec_ar[k, t]
+        
+        t += 1
+    t = 0
+    k += 1
+
+print(thrd_ar)
