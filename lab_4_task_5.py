d21544b093043b5e3788f05f4d549e698d66cd82
JackEDay <111586133+JackEDay@users.noreply.github.com>
2023-12-04 20:55:57 +0000

    21342

--- lab_4_task_5.py
+++ lab_4_task_5.py
@@ -1,4 +1,25 @@
 import numpy as np
 
 def space():
-    chose
+    option = input("Площадь какой фигуры найти? \n Окружность    Треугольник     Прямоугольник \n '1'    '2'     '3'")
+
+    if option == 1:
+        r = int(input("Длина радиуса:   "))
+        S = np.pi * r ** 2
+    
+    elif option == 2:
+        a = int(input("Длина основания:     "))
+        h = int(input("Длина высоты:     "))
+        S = (a * h) / 2
+
+    elif option == 3:
+        a = int(input("Длина первой стороны:     "))
+        b = int(input("Длина второй стороны:     "))
+        S = a * b
+    
+    else:
+        S = "Ошибка. Неверный вариант."
+
+    return S
+
+print(space())
