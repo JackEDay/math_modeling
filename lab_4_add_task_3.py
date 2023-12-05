7114faaccbcaaf59fa59bf3a238f32fb241b3458
JackEDay <111586133+JackEDay@users.noreply.github.com>
2023-12-05 12:26:07 +0000

    321

--- lab_4_add_task_3.py
+++ lab_4_add_task_3.py
@@ -12,6 +12,9 @@ def ai(lens_type, d, f):
     if lens_type == 'Рассеивающая':
         image_type = 'Мнимое, прямое'
 
+    return image_type
+
+print(ai('Собирающая', 12, 5))
 #Ссылка для решения
 #https://www.google.ru/search?q=%D0%BC%D0%BD%D0%B8%D0%BC%D0%BE%D0%B5+%D0%B8%D0%BB%D0%B8+%D0%B4%D0%B5%D0%B9%D1%81%D1%82%D0%B2%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D0%B5+%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5&newwindow=1&safe=strict&sca_esv=588006686&rlz=1C1GCEU_ruRU999RU999&tbm=isch&source=lnms&sa=X&ved=2ahUKEwjb-YG6oviCAxUQHxAIHcRTBlQQ_AUoAXoECAEQAw&biw=1920&bih=963&dpr=1#imgrc=b1q6hIR_91mn-M
         
