f31a3eb6cc611c084c36984d9905252bee53b87c
JackEDay <111586133+JackEDay@users.noreply.github.com>
2023-12-05 10:47:11 +0000

    321

--- lab_4_task_1.py
+++ lab_4_task_1.py
@@ -1,9 +1,10 @@
 import numpy as np
 
-custom_ar = np.array([2, 3, 4, 5, 6])
 
 def task1(ar):
     avr = np.average(ar)
     return avr
 
+
+custom_ar = np.array([2, 3, 4, 5, 6])
 print(task1(custom_ar))
--- lab_4_task_2.py
+++ lab_4_task_2.py
@@ -1,18 +1,13 @@
 import numpy as np
 
-def task2():
-    n = int(input("Введите кол-во элементов массива:    "))
-    ar = np.zeros([1, n])
+ar = np.array([2, 4, 6, 8, 9])
+
+def task2(ar):
     mult = 1
 
-    for i in range (0, n):
-        in_numb = int(input("Введите элемент:   "))
-        ar[0, i] = in_numb
-        print(ar)
-    
-    for j in range (0, n):
-        mult = mult * ar[0, j]
+    for i in range (0, 5):
+        mult = mult * ar[i]
 
-    return(mult)
+    return mult
 
-print(task2())
+print(task2(ar))
