96b66076c91e076afdfa0efd8397da4a701cfd9a
JackEDay <111586133+JackEDay@users.noreply.github.com>
2023-12-05 12:22:16 +0000

    312

--- lab_4_add_task_3.py
+++ lab_4_add_task_3.py
@@ -3,8 +3,13 @@ def ai(lens_type, d, f):
         if d < f:
             image_type = 'Мнимое, увеличенное, прямое.'
         elif d > 2 * f:
-            image_type = 'Действительное, '
+            image_type = 'Действительное, уменьшенное, перевёрнутое'
+        elif d < 2 * f and d > f:
+            image_type = 'Действительное, увеличенное, перевёрнутое'
         else:
             image_type = 'Нет изображения'
     
+
+#Ссылка для решения
+#https://www.google.ru/search?q=%D0%BC%D0%BD%D0%B8%D0%BC%D0%BE%D0%B5+%D0%B8%D0%BB%D0%B8+%D0%B4%D0%B5%D0%B9%D1%81%D1%82%D0%B2%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D0%B5+%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5&newwindow=1&safe=strict&sca_esv=588006686&rlz=1C1GCEU_ruRU999RU999&tbm=isch&source=lnms&sa=X&ved=2ahUKEwjb-YG6oviCAxUQHxAIHcRTBlQQ_AUoAXoECAEQAw&biw=1920&bih=963&dpr=1#imgrc=b1q6hIR_91mn-M
         
