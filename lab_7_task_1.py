821503f24fd26559e736e52d36a3ba88cabda236
Seregachek <111586133+JackEDay@users.noreply.github.com>
2024-02-24 13:11:25 +0000

    231

--- lab_7_task_1.py
+++ lab_7_task_1.py
@@ -1,8 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-def draw(a):
+def draw_cycloid(a):
     t = np.arange(-2*np.pi, 2*np.pi, 0.1)
     x = a * (t - np.sin(t))
     y = a * (1 - np.cos(t))
@@ -10,6 +9,17 @@ def draw(a):
     plt.axis('equal')
     plt.grid()
     plt.plot(x, y)
-    plt.savefig('task_1.png')
+    plt.savefig('task_cycloid.png')
 
-draw(1)
+def draw_astroid(R, t):
+    t = np.arange(-2*np.pi, 2*np.pi, 0.1)
+    x = R * np.cos(t)**3
+    y = R * np.sin(t)**3
+
+    plt.axis('equal')
+    plt.grid()
+    plt.plot(x, y)
+    plt.savefig('task_astroid.png')
+
+#draw_cycloid(1)
+draw_astroid(2, 1)
---
+++ lab_7_task_2.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
+
+def circle(a, t):
+    R = a * t
+    alpha = np.arange(0, 2*np.pi, 0.05)
+    x = R*np.cos(alpha)
+    y = R*np.sin(alpha)
+    return x, y
+
+def animate(i):
+    ball.set_data(circle(a = 0.05, t=i))
+
+if __name__ == '__main__':
+    fig, ax = plt.subplots()
+    ball, = plt.plot([], [], 'o', color='r', label='Ball')
+
+    edge = 3
+    plt.axis('equal')
+
+    ax.set_xlim(-edge, edge)
+    ax.set_ylim(-edge, edge)
+
+    ani = FuncAnimation(fig, 
+                        animate,
+                        frames=20,
+                        interval=50
+                        )
+    
+    ani.save('Lab_7_task_2.gif')
