3a961642e64ee9bd96b09ce6112ae65b2e87911f
Seregachek <111586133+JackEDay@users.noreply.github.com>
2024-04-01 22:35:14 +0000

    131

--- project.py
+++ project.py
@@ -12,14 +12,14 @@ def planet(R, angle_vel, t):
     return x, y
 
 def animate(i):
-    mercury.set_data(planet(R=2.1, angle_vel=4.14772, t=i+20))       #Year = 88 days
-    venus.set_data(planet(R=3.6, angle_vel=1.62222, t=i+100))        #Year = 225 days
+    mercury.set_data(planet(R=2.1, angle_vel=4.14772, t=i))       #Year = 88 days
+    venus.set_data(planet(R=3.6, angle_vel=1.62222, t=i))        #Year = 225 days
     earth.set_data(planet(R=5.5, angle_vel=1, t=i))                  #Year = 365    
-    mars.set_data(planet(R=7, angle_vel=0.53129, t=i+350))           #Year = 687
-    jupiter.set_data(planet(R=9, angle_vel=0.08423, t=i+3700))       #Year = 4333 days
-    saturn.set_data(planet(R=11, angle_vel=0.03392, t=i+2400))       #Year = 10 759 days
-    uranus.set_data(planet(R=12, angle_vel=0.01189, t=i+25000))      #Year = 30 687 days    
-    neptune.set_data(planet(R=11, angle_vel=0.00606, t=i+35000))     #Year = 60 190 days
+    mars.set_data(planet(R=7, angle_vel=0.53129, t=i))           #Year = 687
+    jupiter.set_data(planet(R=8.5, angle_vel=0.08423, t=i))       #Year = 4333 days
+    saturn.set_data(planet(R=9.8, angle_vel=0.03392, t=i))       #Year = 10 759 days
+    uranus.set_data(planet(R=11, angle_vel=0.01189, t=i))      #Year = 30 687 days    
+    neptune.set_data(planet(R=12, angle_vel=0.00606, t=i))     #Year = 60 190 days
     moon.set_data(moonplt(R=1.2, angle_vel=5, t=i))
 
     
@@ -53,7 +53,7 @@ if __name__ == '__main__':
 
     ani = FuncAnimation(fig, 
                         animate,
-                        frames=1500,
-                        interval=30
+                        frames=365,
+                        interval=50
                         )
-    ani.save('Project_Usual_position.gif')
+    ani.save('Project_cycle_start.gif')
